a2afbe95073758da364af3deaac3287bea3c7068
Brian Li <Brian.Li96@yahoo.com>
2020-11-24 11:55:27 -0600

    Done

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,48 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def specific_gym
+    Membership.all.select do |memberships|
+      memberships.gym == self
+    end
+  end
+
+  def all_lifters_membership
+    list_of_lifters = []
+    Membership.all.each do |specific|
+      if specific.gym == self
+        list_of_lifters << specific.lifter
+      end
+    end
+    list_of_lifters
+  end
+
+  def names_of_lifters
+    array_of_names = []
+    Membership.all.each do |names|
+      if names.gym == self
+        array_of_names << names.lifter.name
+      end
+    end
+    array_of_names
+  end
+
+  def combined_lift_total
+    combined_total = 0
+      self.all_lifters_membership.each do |lifters|
+        combined_total += lifters.lift_total
+      end
+    combined_total
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,54 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def lifter_membership
+    Membership.all.select do |pass|
+      pass.lifter == self
+    end  
+  end
+
+  def gym_memberships
+    list_of_gyms = []
+    Membership.all.each do |specific|
+      if specific.lifter == self
+        list_of_gyms << specific.gym
+      end
+    end
+    list_of_gyms
+  end
+
+  def self.average_lift 
+    total_average = 0
+    self.all.each do |lifter|
+      total_average += lifter.lift_total
+    end 
+    total_average = total_average / self.all.size
+    total_average
+  end
+
+  def total_cost_of_lifter
+    total_cost = 0
+    Membership.all.each do |memberships|
+      if memberships.lifter == self
+        total_cost += memberships.cost
+      end
+    end
+    total_cost
+  end
+
+  def sign_up(gym, cost)
+    new_membership = Membership.new(self, gym, cost)
   end
 end
